d77da7598805b89e7e48fc3223c090db755ae90f
Nishant Gada <49140662+NishantGada@users.noreply.github.com>
2024-01-31 18:26:15 -0500

    Improved code by adding Object Oriented Design and more comments

--- main.py
+++ main.py
@@ -1,51 +1,76 @@
 import math
-from time import process_time 
+from time import process_time
 
-t1_start = process_time()
-input = [(0.78, 1.47, 1.77, 3.92), ("s1", "s2", "e1", "e2")]
-# input = [(0.9, 1.3, 1.70, 2.92), ("s1", "e1", "s2", "e2")]
+class LineIntersection:
+    def __init__(self, points):
+        self.points = points
+        self.mydict = {}
+        # co-ordinates dictionary
+        self.coords = {}
+        # maintaining the count of all intersections inside the circle
+        self.count = 0
 
-mydict = {}
-coords = {}
-count = 0
+    # getting the co-ordinates based on the input points, using Trigonometry
+    def convert_to_coords(self):
+        for i in self.points[1]:
+            self.mydict[i] = self.points[0][self.points[1].index(i)]
 
-for i in input[1]:
-    mydict[i] = input[0][input[1].index(i)]
+        for i in self.mydict:
+            x, y = math.cos(self.mydict[i]), math.sin(self.mydict[i])
+            self.coords[i] = x, y
 
-for i in mydict:
-    # generating x & y co-ordinates based on the radian values using Trigonometry
-    x, y = math.cos(mydict[i]), math.sin(mydict[i])
-    coords[i] = x, y
+    # calculating the slopes of both the chords using mathematical concepts of co-ordinate geometry
+    # formula being used:
+        # m (slope) = (y2 - y1) / (x2 - x1)
+    def calculate_slopes(self):
+        slope1 = (self.coords["s1"][1] - self.coords["e1"][1]) / (self.coords["s1"][0] - self.coords["e1"][0])
+        slope2 = (self.coords["s2"][1] - self.coords["e2"][1]) / (self.coords["s2"][0] - self.coords["e2"][0])
+        return slope1, slope2
 
-slope1 = (coords["s1"][1] - coords["e1"][1]) / (coords["s1"][0] - coords["e1"][0])
-slope2 = (coords["s2"][1] - coords["e2"][1]) / (coords["s2"][0] - coords["e2"][0])
+    # after calculating the slopes, we check for the intersection and calculate its co-ordinates
+    def check_intersection(self, slope1, slope2):
+        if slope1 != slope2:
+            print("Not Parallel, checking further.. ")
 
-if slope1 != slope2:
-    print("Not Parallel, checking further.. ")
+            # formula being used:
+                # x = (b2 - b1) / (m1 - m2)
+                # y = m1 (x - x1) + y1
+            x_intersection = (slope1 * self.coords["s1"][0] - self.coords["s1"][1] - slope2 * self.coords["s2"][0] + self.coords["s2"][1]) / (slope1 - slope2)
+            y_intersection = slope1 * (x_intersection - self.coords["s1"][0]) + self.coords["s1"][1]
 
-    # Calculating the intersection point based on the following mathematical formula
-    # x = (b2 - b1) / (m1 - m2)
-    # and, y = m1 (x - x1) + y1
-    x_intersection = (slope1 * coords["s1"][0] - coords["s1"][1] - slope2 * coords["s2"][0] + coords["s2"][1]) / (slope1 - slope2)
-    y_intersection = slope1 * (x_intersection - coords["s1"][0]) + coords["s1"][1]
+            # checking whether the intersection takes place inside or outside the circle
+            if all(
+                min(self.coords[point][0], self.coords[point2][0]) <= x_intersection <= max(self.coords[point][0], self.coords[point2][0]) and
+                min(self.coords[point][1], self.coords[point2][1]) <= y_intersection <= max(self.coords[point][1], self.coords[point2][1])
+                for point, point2 in [("s1", "e1"), ("s2", "e2")]
+            ):
+                # incrementing the intersection count if it takes place inside the circle
+                print("Lines Intersect at Point:", (x_intersection, y_intersection))
+                self.count += 1
+            else:
+                print("Intersection point falls outside the circle")
+                print("Intersection Co-ordinates:", (x_intersection, y_intersection))
+        else:
+            print("Parallel Lines, No Intersection")
 
-    # Checking if the intersection point lies within our required bounds
-    if (
-        min(coords["s1"][0], coords["e1"][0]) <= x_intersection <= max(coords["s1"][0], coords["e1"][0]) and
-        min(coords["s1"][1], coords["e1"][1]) <= y_intersection <= max(coords["s1"][1], coords["e1"][1]) and
-        min(coords["s2"][0], coords["e2"][0]) <= x_intersection <= max(coords["s2"][0], coords["e2"][0]) and
-        min(coords["s2"][1], coords["e2"][1]) <= y_intersection <= max(coords["s2"][1], coords["e2"][1])
-    ):
-        print("Lines Intersect at Point: ", (x_intersection, y_intersection))
-        count += 1
-    else:
-        print("Intersection point falls outside the circle")
-        print("Intersection Co-ordinates: ", (x_intersection, y_intersection))
-else:
-    print("Parallel Lines, No Intersection")
+    # printing the final count of intersections taking place inside the circle
+    def print_intersection_count(self):
+        print("Number of Intersections:", self.count)
 
-print("Number of Intersections: ", count)
 
-t1_stop = process_time()
-d = t1_stop - t1_start
-print("Computing all Intersections took:", d * 1000, " milliseconds")
+if __name__ == "__main__":
+    t1_start = process_time()
+
+    # Creating an instance of LineIntersection with the following input
+    line_intersection = LineIntersection([(0.78, 1.47, 1.77, 3.92), ("s1", "s2", "e1", "e2")])
+
+    # Calling the necessary methods
+    line_intersection.convert_to_coords()
+    slope1, slope2 = line_intersection.calculate_slopes()
+    line_intersection.check_intersection(slope1, slope2)
+    line_intersection.print_intersection_count()
+
+    # calculating the program runtime
+    t1_stop = process_time()
+    d = t1_stop - t1_start
+    print("Computing all Intersections took:", d * 1000, "milliseconds")
